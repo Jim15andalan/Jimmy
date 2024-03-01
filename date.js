e019667e12cda06fb060dc8fe86d51212db309c6
Jim15andalan <161802291+Jim15andalan@users.noreply.github.com>
2024-03-01 15:37:51 +0800

    Add files via upload

---
+++ date.js
@@ -0,0 +1,10 @@
+function getCurrentDateTime() {
+    const currentDateTime = new Date();
+    const options = {weekday: `long`,year:
+    `numeric`, month: `long`, day:'numeric',hour: `numeric`,minute: `numeric`, second:`numeric`, hour12: true };
+    return currentDateTime.toLocaleString('en-US', options);
+    
+}
+
+document.getElementById("date").textContent =getCurrentDateTime();
+    
